b34cd9d8a1898468955008ab34c3304d89c5d9e8
Vivek Kasaudhan <vivekkasaudhan241@gmail.com>
2026-02-15 16:21:42 +0530

    Add userRoutes and cookieParser to index.js

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,18 @@
 import express from "express";
+import adminRoutes from "./routes/adminRoutes.js";
+import userRoutes from "./routes/UserRoutes.js";
 import societyRoutes from "./routes/societyRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
-
+app.use(cookieParser());
 app.use(express.json());
 
 // base path set
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", userRoutes);
 app.use("/api/society", societyRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
